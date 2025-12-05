49f07c7062a8d6ba30cc78d5f703bd1c000c5c85
Isaiah Bullard <isaiah0812@yahoo.com>
2025-12-05 15:12:43 -0700

    formatted time markers

--- client/src/components/timeSeeker.tsx
+++ client/src/components/timeSeeker.tsx
@@ -5,16 +5,24 @@ interface TimeSeekerProps {
 }
 
 export default function TimeSeeker({ song }: TimeSeekerProps) {
+  const getStringTime = (time: number): string => {
+    const totalSeconds = Math.floor(time);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    return `${minutes}:${(seconds < 10 ? '0' : '') + seconds}`
+  }
+  
   const [ duration, setDuration ] = useState(song.duration);
   const [ currentTime, setCurrentTime ] = useState(song.currentTime);
 
-  song.onloadeddata = () => setDuration(song.duration);
+  song.onloadeddata = () => setDuration(song.duration)
   song.ontimeupdate = () => setCurrentTime(song.currentTime);
   
   return (
     <>
-      {currentTime}/{duration}
-      <input type="range" name="timeSeeker" min="0" max={duration} value={currentTime} onChange={(e) => {
+      {getStringTime(currentTime)}/{getStringTime(duration)}
+      <input type="range" name="timeSeeker" min="0" max={duration.toString()} value={currentTime} onChange={(e) => {
         const newTime = parseFloat(e.target.value)
         song.fastSeek(newTime);
         setCurrentTime(newTime)
