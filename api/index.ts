1ed3d6fceab1b4dcaa7060c3dd85dd0415c3ddd6
Isaiah Bullard <isaiah0812@yahoo.com>
2026-01-18 13:07:24 -0700

    created /stream and /download endpoints, thickened player border

--- api/index.ts
+++ api/index.ts
@@ -4,9 +4,8 @@ import { createReadStream } from 'node:fs';
 
 const app = express();
 
-app.get('/audio', (req: Request, res: Response) => {
-  createReadStream(path.resolve('./assets/Dont_Go_Way_Nobody.mp3')).pipe(res);
-});
+app.get('/audio/stream', (req: Request, res: Response) => createReadStream(path.resolve('./assets/Dont_Go_Way_Nobody.mp3')).pipe(res));
+app.get('/audio/download', (req: Request, res: Response) => res.sendFile(path.resolve('./assets/file_example_WAV_1MG.wav')));
 
 app.listen(8080, () => {
   console.log('Hello World!');
