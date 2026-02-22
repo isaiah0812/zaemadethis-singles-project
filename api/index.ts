2cfda9f4974eca447f500befc6030159e7c88f94
Isaiah Bullard <isaiah0812@yahoo.com>
2026-02-22 09:55:28 -0700

    add cors, download audio from server

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import path from 'node:path';
 import { createReadStream } from 'node:fs';
+import cors from 'cors';
 
 const app = express();
+app.use(cors())
 
 app.get('/audio/stream', (req: Request, res: Response) => createReadStream(path.resolve('./assets/Dont_Go_Way_Nobody.mp3')).pipe(res));
 app.get('/audio/download', (req: Request, res: Response) => res.sendFile(path.resolve('./assets/file_example_WAV_1MG.wav')));
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import DownloadButton from './components/downloadButton';
 import TimeSeeker from './components/timeSeeker';
 import VolumeSlider from './components/volumeSlider';
 
 function App() {
-  const song = new Audio('Dont_Go_Way_Nobody.mp3');
+  const [ song, setSong ] = useState<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/audio/stream')
+      .then((response) => {
+        console.log(response);
+
+        setSong(new Audio('Dont_Go_Way_Nobody.mp3'));
+      }).catch(err => console.error(err));
+  }, [])
   
   return (
     <>
-      <div id="player">
-        <TimeSeeker song={song} />
-        <div id="second-row">
-          <VolumeSlider song={song} />
-          <DownloadButton />
+      {song ? 
+        <div id="player">
+          <TimeSeeker song={song} />
+          <div id="second-row">
+            <VolumeSlider song={song} />
+            <DownloadButton />
+          </div>
         </div>
-      </div>
+      : 'No Audio'}
     </>
   )
 }
