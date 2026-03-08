da2ee56736046fa8308414bbccad0d98b2a8fc05
Isaiah Bullard <isaiah0812@yahoo.com>
2026-03-08 12:27:56 -0600

    basic download functionality, write id3 tags on startup, mp3 only (for now), vulnerability cleanup in server

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,24 @@
 import express, { Request, Response } from 'express';
 import path from 'node:path';
-import { createReadStream } from 'node:fs';
+import { createReadStream, readFileSync } from 'node:fs';
 import cors from 'cors';
+import id3 from 'node-id3';
 
 const app = express();
-app.use(cors())
+app.use(cors());
+
+const downloadedFile = path.resolve('./assets/Dont_Go_Way_Nobody_with_tags.mp3');
+const tagsFile = path.resolve('./assets/tags.json');
+const id3File = id3.update(JSON.parse(readFileSync(tagsFile).toString()), downloadedFile)
+
+if (id3File === true) {
+  console.log('Tags written to file!');
+} else {
+  console.error(id3File.message);
+}
 
 app.get('/audio/stream', (req: Request, res: Response) => createReadStream(path.resolve('./assets/Dont_Go_Way_Nobody.mp3')).pipe(res));
-app.get('/audio/download', (req: Request, res: Response) => res.download(path.resolve('./assets/file_example_WAV_1MG.wav')));
+app.get('/audio/download', (req: Request, res: Response) => res.download(downloadedFile));
 
 app.listen(8080, () => {
   console.log('Hello World!');
--- client/src/components/downloadButton.tsx
+++ client/src/components/downloadButton.tsx
@@ -2,8 +2,9 @@ import { mdiTrayArrowDown } from "@mdi/js";
 import Icon from "@mdi/react";
 
 export default function DownloadButton() {
+  const downloadSong = () => window.location.assign('http://localhost:8080/audio/download')
   return (
-    <button id="download-button" onClick={() => alert('Song downloading!')}>
+    <button id="download-button" onClick={downloadSong}>
       <Icon path={mdiTrayArrowDown} size="2rem" />
     </button>
   )
