2ccf2a61b3ddf1c5735a0274cfaeabbb095b70fa
Isaiah Bullard <isaiah0812@yahoo.com>
2026-09-08 16:09:08 -0600

    template for the next page for payments

--- client/src/components/payment.tsx
+++ client/src/components/payment.tsx
@@ -6,11 +6,16 @@ type PaymentProps = {
   onSuccess: () => any;
 }
 
+type PaymentState = 'selection' | 'checkout' | null;
+
 export default function PaymentSelection({ close, onSuccess }: PaymentProps) {
   const [ price, setPrice ] = useState<string>('0.00');
+  const [ state, setState ] = useState<PaymentState>('selection');
+
   const handleClose = (event: any) => {
     event.preventDefault();
     if (event.target === event.currentTarget) {
+
       close();
     }
   }
@@ -33,21 +38,38 @@ export default function PaymentSelection({ close, onSuccess }: PaymentProps) {
   const handlePayment = () => {
     // TODO send price to payment screen
 
+    setState(null);
     close();
     onSuccess();
   }
+
   return(
     <div className="modal-overlay" onClick={handleClose}>
-      <div className="payment-modal">
-        <h3>Pay What You Want!</h3>
-        <p>I really don't care if you pay anything at all. It's free.
-          But, if you're feeling like this shouldn't be free, let the
-          intrusive thoughts win this time, enter an amount below,
-          then click "Next".</p>
-        <input type='number' placeholder="0.00" value={price} onChange={handlePriceChange} />
-        <button style={{backgroundColor: 'blue'}} onClick={handlePayment}>Next</button>
-        <button style={{backgroundColor: 'red'}} onClick={close}>Cancel</button>
-      </div>
+      {state === 'selection' && (
+        <div className="payment-selection-modal">
+          <h3>Pay What You Want!</h3>
+          <p>I really don't care if you pay anything at all. It's free.
+            But, if you're feeling like this shouldn't be free, let the
+            intrusive thoughts win this time, enter an amount below,
+            then click "Next".</p>
+          <input type='number' placeholder="0.00" value={price} onChange={handlePriceChange} />
+          <button style={{backgroundColor: 'blue'}} onClick={() => setState('checkout')}>Next</button>
+          <button style={{backgroundColor: 'red'}} onClick={() => {
+            setState(null);
+            close();
+          }}>Cancel</button>
+        </div>
+      )}
+      {state === 'checkout' && (
+        <div className="payment-checkout-modal">
+          Hey there!
+          <button style={{backgroundColor: 'blue'}} onClick={handlePayment}>Next</button>
+          <button style={{backgroundColor: 'red'}} onClick={() => {
+            setState(null);
+            close();
+          }}>Cancel</button>
+        </div>
+      )}
     </div>
   )
 }
